043d0634dc34272853ec804398fc564b8058dfa8
Ekara Daniel <daniel.ekara@yahoo.com>
2022-04-15 00:37:49 +0100

    Initial commit

---
+++ src/main/java/kara/spotifyassistant/SpotifyAssistantApplication.java
@@ -0,0 +1,13 @@
+package kara.spotifyassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kara/spotifyassistant/SpotifyAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package kara.spotifyassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
