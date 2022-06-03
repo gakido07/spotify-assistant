4662bac386b1e14fbdf13933a5b13478562d1ef7
Ekara Daniel <daniel.ekara@yahoo.com>
2022-06-03 10:48:03 +0100

    Added Cors Mapping

--- src/main/java/kara/spotifyassistant/SpotifyAssistantApplication.java
+++ src/main/java/kara/spotifyassistant/SpotifyAssistantApplication.java
@@ -2,7 +2,10 @@ package kara.spotifyassistant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -11,5 +14,4 @@ public class SpotifyAssistantApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpotifyAssistantApplication.class, args);
 	}
-
 }
