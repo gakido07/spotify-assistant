016a0cc01ef7f38b79d27e11a564cde15403db1e
Ekara Daniel <daniel.ekara@yahoo.com>
2022-06-04 09:35:26 +0100

    Revert "Added Cors Mapping"
    This reverts commit 4662bac386b1e14fbdf13933a5b13478562d1ef7.

--- src/main/java/kara/spotifyassistant/SpotifyAssistantApplication.java
+++ src/main/java/kara/spotifyassistant/SpotifyAssistantApplication.java
@@ -2,10 +2,7 @@ package kara.spotifyassistant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -14,4 +11,5 @@ public class SpotifyAssistantApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpotifyAssistantApplication.class, args);
 	}
+
 }
